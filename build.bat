02dac9455b85d8f4c23ae2454c02a157180ce479
Marco Marini <mmarini@tai.it>
2026-06-24 11:48:02 +0200

    Update build script for dynamic jar naming, replace Jackson with SQLite dependency, and configure OpenFeign settings.

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ IF EXIST %DIST_DIR% RMDIR /S /Q %DIST_DIR%
 MKDIR %DIST_DIR%\log
 MKDIR %DIST_DIR%\config
 CALL mvn package -DskipTests -Dlicense.skip=true
-COPY target\ranp.jar %DIST_DIR%
+COPY target\ranp-*.jar %DIST_DIR%
 COPY src\main\resources\application.yml %DIST_DIR%\config
 COPY src\main\resources\example*.json %DIST_DIR%
