13f50c2a4b8d6b973db4b8795b590d3a06fec6cd
Marco Marini <mmarini@tai.it>
2026-06-24 11:22:59 +0200

    Rename project to "RANP" and add Liquibase support with initial database schema for node IDs.

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 
-SET DIST_DIR=anp
+SET DIST_DIR=ranp
 
 IF EXIST %DIST_DIR% RMDIR /S /Q %DIST_DIR%
 MKDIR %DIST_DIR%\log
 MKDIR %DIST_DIR%\config
 CALL mvn package -DskipTests -Dlicense.skip=true
-COPY target\anp.jar %DIST_DIR%
+COPY target\ranp.jar %DIST_DIR%
 COPY src\main\resources\application.yml %DIST_DIR%\config
 COPY src\main\resources\example*.json %DIST_DIR%
